87a78129bcb4a534b70ff9907c40c742483e3bac
Lucas107-sudo <lucaspaulo555@outlook.com>
2025-02-27 22:24:58 +0000

    Desafio Completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,81 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+char estado[50], estado2[50];
+char codigo[50], codigo2[50];
+char cidade[50], cidade2[50];
+int populacao, populacao2;
+float areakm, areakm2;
+float pib, pib2;
+int pontoturistico, pontoturistico2;
 
-    return 0;
+//Obtendo as características da carta 01
+
+printf("Digite o estado: \n");
+scanf("%s", &estado);
+
+printf("Digite o código da carta: \n");
+scanf("%s", &codigo);
+
+printf("Digite a cidade: \n");
+scanf("%s", &cidade);
+
+printf(" Digite a população: \n");
+scanf("%d", &populacao);
+
+printf(" Digite sua área em Km²: \n");
+scanf("%f", &areakm),
+
+printf("Digite o PIB: \n");
+scanf("%f", &pib);
+
+printf("Digite a quantidade de pontos turísticos: \n");
+scanf("%d", &pontoturistico);
+
+
+//Características da carta 01
+
+printf("O estado é: %s - Código: %s\n", estado, codigo);
+printf("A cidade é: %s - Com uma população de: %d milhões de pessoas\n", cidade, populacao);
+printf(" Sua área em Km² é de: %.3f - Com um PIB de: %.2f\n", areakm, pib);
+printf(" Contando com: %.d pontos turísticos\n", pontoturistico);
+
+
+
+
+//Obtendo as caracteristicas da carta 02
+
+printf("Digite o estado: \n");
+scanf("%s", estado2);
+
+printf("Digite o código da carta: \n");
+scanf("%s", codigo2);
+
+printf("Digite a cidade: \n");
+scanf("%s", cidade2);
+
+printf(" Digite a população: \n");
+scanf("%d", &populacao2);
+
+printf(" Digite sua área em Km²: \n");
+scanf("%f", &areakm2),
+
+printf("Digite o PIB: \n");
+scanf("%f", &pib2);
+
+printf("Digite a quantidade de pontos turísticos: \n");
+scanf("%d", &pontoturistico2);
+
+//Características da carta 02
+
+printf("O estado é: %s - Código: %s\n", estado2, codigo2);
+printf("A cidade é: %s - Com uma população de: %d milhões de pessoas\n", cidade2, populacao2);
+printf(" Sua área em Km² é de: %.3f - Com um PIB de: %.2f\n", areakm2, pib2);
+printf(" Contando com: %.d pontos turísticos\n", pontoturistico2);
+
+
+return 0;
+
+
 }
